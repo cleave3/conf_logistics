70074c63121e54d869b17ea2ea9385b15cbcb1af
cleave3 <owhiroroeghele@gmail.com>
2021-02-03 20:11:38 +0100

    add more nav items

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd324f69826c936fea7d6d8cd3e3b98d7b199af2e',
+    'name' => 'ail/conf_logistics',
+  ),
+  'versions' => 
+  array (
+    'ail/conf_logistics' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd324f69826c936fea7d6d8cd3e3b98d7b199af2e',
+    ),
+    'firebase/php-jwt' => 
+    array (
+      'pretty_version' => 'v5.2.0',
+      'version' => '5.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'feb0e820b8436873675fd3aca04f3728eb2185cb',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.2.0',
+      'version' => '6.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e38888a75c070304ca5514197d4847a59a5c853f',
+    ),
+  ),
+);
