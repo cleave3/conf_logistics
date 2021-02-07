f22e0b9c876e50c21e38415e003233c1d41383fe
cleave3 <owhiroroeghele@gmail.com>
2021-02-07 08:55:30 +0100

    add paper js

--- views/clients/common/js.php
+++ views/clients/common/js.php
@@ -7,6 +7,6 @@
 <!--  Notifications Plugin    -->
 <script src="/assets/js/plugins/bootstrap-notify.js"></script>
 <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-<!-- <script src="/assets/js/paper-dashboard.min.js" type="text/javascript"></script> -->
+<script src="/assets/js/paper-dashboard.min.js" type="text/javascript"></script>
 <script src="/assets/js/datatables.min.js" type="text/javascript"></script>
 <script src="/assets/js/main.js" type="text/javascript"></script>
